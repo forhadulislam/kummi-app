485d085a39a14af524ca88987cab0e53742cab61
Kennedy Opoku Asare <opokuasarekennedy@gmail.com>
2017-03-08 20:58:37 +0200

    Bottom Navigation

--- app/src/main/java/fi/oulu/mobisocial/kummi_application/MainActivity.java
+++ app/src/main/java/fi/oulu/mobisocial/kummi_application/MainActivity.java
@@ -1,13 +1,54 @@
 package fi.oulu.mobisocial.kummi_application;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+    private BottomNavigationView bottomNavigation;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigation = (BottomNavigationView)findViewById(R.id.navigation);
+        bottomNavigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                selectMenuItem(item);
+
+                return  true;
+
+            }
+
+
+        });
+
+    }
+
+    private void selectMenuItem(MenuItem item) {
+        switch (item.getItemId()) {
+
+        }
+
+        // uncheck the other items.
+        for (int i = 0; i< bottomNavigation.getMenu().size(); i++) {
+            MenuItem menuItem = bottomNavigation.getMenu().getItem(i);
+            menuItem.setChecked(menuItem.getItemId() == item.getItemId());
+        }
+
+        updateToolbarText(item.getTitle());
+
+    }
+
+    private void updateToolbarText(CharSequence text) {
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setTitle(text);
+        }
     }
 }
